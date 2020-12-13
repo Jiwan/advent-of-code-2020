bf7b91b6c999a32a199ec150e0f8ce6f64f96ff4
Jean Guegant <jean.guegant@king.com>
2020-12-13 21:45:53 +0100

    Day 7 part 1 done.

--- app/Day7/Main.hs
+++ app/Day7/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import qualified Text.Parsec as Parsec
-import Control.Monad 
+import Control.Monad () 
+import Data.Either (fromRight)
+import qualified Data.Set as Set
 
 smallestBag = do
     Parsec.space
@@ -15,32 +17,40 @@ parseBagName = do
     Parsec.skipMany (Parsec.char 's')
     return name
 
-containedBag = do
+parseContainedBag = do
     Parsec.space
     digits <- (Parsec.many1 Parsec.digit)
     Parsec.space
     name <- parseBagName
     return (name, read digits :: Int)
 
-containedBags = Parsec.sepBy containedBag (Parsec.char ',')
+parseContainedBags = Parsec.sepBy parseContainedBag (Parsec.char ',')
 
-bagLine :: Parsec.Parsec String () (String, [(String, Int)])
-bagLine = do
+parseBagLine :: Parsec.Parsec String () (String, [(String, Int)])
+parseBagLine = do
     bag <- parseBagName
     Parsec.space
     Parsec.string "contain"
-    contained <- Parsec.choice [Parsec.try smallestBag, containedBags]
+    contained <- Parsec.choice [Parsec.try smallestBag, parseContainedBags]
     return (bag, contained) 
 
 eof = do 
     Parsec.eof
     return ("",[]) 
 
-bags = do
-    Parsec.sepBy (Parsec.choice [bagLine, eof]) (Parsec.string ".\n")
+parseBags = do
+    Parsec.sepBy (Parsec.choice [parseBagLine, eof]) (Parsec.string ".\n")
+
+findAllContainers :: [(String, [(String, Int)])] -> Set.Set String -> Set.Set String -> Set.Set String
+findAllContainers rules toLook found
+    | Set.null toLook = found
+    | otherwise = let newToLook = map fst $ filter (\(_, subbags) -> not . Set.null $ Set.intersection toLook $ Set.fromList (map fst subbags)) rules in  
+        findAllContainers rules (Set.fromList newToLook) (toLook `Set.union` found) 
 
 main :: IO ()
 main = do
     putStrLn "Day 7"
     file <- readFile "data/test7.txt"
-    print $ Parsec.parse bags "test" file
+    let rules = fromRight [] $ Parsec.parse parseBags "bags" file
+    print $ (-1 +) $ length $ findAllContainers rules (Set.fromList ["shiny gold"]) Set.empty
+
