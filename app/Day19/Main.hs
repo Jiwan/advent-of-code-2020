b2a04a2a29a24ab50fc06a290a9b19e64d238d0f
Jean Guegant <jean.guegant@king.com>
2020-12-21 14:05:53 +0100

    Cleaner solution.

--- app/Day19/Main.hs
+++ app/Day19/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.List.Split (splitOn)
 import qualified Data.Map as Map
-import Data.Maybe (isJust, fromJust)
+import Data.Maybe (catMaybes, isJust, fromJust)
 import Control.Monad (msum)
 import Debug.Trace (trace)
 import Data.List (sortBy)
@@ -28,53 +28,29 @@ parseInput file =
     in
         (parseRules $ head l, head (tail l))
 
-executeRule :: Map.Map Int [[Op]] -> [Char] -> Op  -> [String] -> Maybe [Char]
-executeRule rulesMap (s:xs) (ReadOp c) backtrace
-    | s == c = trace ("Successfully consuming " ++ [s]) Just xs
-    | otherwise = trace ("Wrong character " ++ [s] ++ " expected " ++ [c]) Nothing
-executeRule rulesMap s (ExecuteRule index) backtrace = trace ("Executing rule " ++ show index ++ "[" ++ show s ++ "]") executeRules rulesMap s (fromJust $ Map.lookup index rulesMap) backtrace
+executeRule :: Map.Map Int [[Op]] -> Op -> [Char] -> [Maybe [Char]]
+executeRule rulesMap (ReadOp c) (s:xs) 
+    | s == c = [Just xs]
+    | otherwise = [Nothing]
+executeRule rulesMap (ExecuteRule index) s = executeOrRules rulesMap (fromJust $ Map.lookup index rulesMap) [s]
 
-executeRules :: Map.Map Int [[Op]] -> [Char] -> [[Op]] -> [String] -> Maybe [Char]
-executeRules _ [] [] backtrace = trace ("The end") Just "" 
-executeRules _ [] [x] backtrace = Nothing 
+executeOrRules :: Map.Map Int [[Op]] -> [[Op]] -> [[Char]] -> [Maybe [Char]]
+executeOrRules rulesMap orRules stringsToCheck
+    | "" `elem` stringsToCheck = []
+    | otherwise = concat $ concatMap (\s -> map (\op -> executeSeqRules rulesMap op s) orRules) stringsToCheck
 
---executeRules rulesMap s [[ExecuteRule 42,ExecuteRule 31], [ExecuteRule 42,ExecuteRule 11,ExecuteRule 31]] backtrace = 
---    case trace ("TADAM!!!") (executeRules rulesMap s [[ExecuteRule 42, ExecuteRule 31]] (backtrace ++ ["TEST"])) of
---        Nothing -> executeRules rulesMap s [[ExecuteRule 42,ExecuteRule 11,ExecuteRule 31]] backtrace
---        result -> trace ("Was there") result
-
-
-executeRules rulesMap s [x, y] backtrace =
-    case trace ("\nCHOICE: " ++ show x ++ "-" ++ show y ++ "\n") (executeRules rulesMap s [x] (backtrace ++ [show y])) of
-        Nothing -> trace ("\nBacktrack " ++ show y ++ "\n") executeRules rulesMap s [y]  backtrace
-        result -> trace ("Was there") result
-
-executeRules rulesMap s [[rule]] backtrace = trace ("YOuhouh") executeRule rulesMap s rule backtrace
-
-executeRules rulesMap s [(rule:restRule)] backtrace =
-    case (executeRule rulesMap s rule backtrace) of
-        Nothing -> trace "baise ouais" Nothing 
-        Just rest ->  executeRules rulesMap rest [restRule] backtrace
-
--- executeRules rulesMap str rules
---     | null str && null rules = Just ""
---     | null str && not (null rules) = Nothing
---     | length rules == 2 = let [first, second] = rules in 
---         let result = trace ("\nCHOICE:" ++ show first  ++ "-" ++ show second ++ "\n" ++ "Testsing with " ++ show first) (executeRules rulesMap str [first]) in
---             case result of
---                 Nothing -> trace ("Failed, will backtrack with " ++ show second) executeRules rulesMap str [second]
---                 _ -> trace ("Was there") result 
---     | length rules == 1 = let (rule:restRules) = head rules in
---         let r = trace ("Executing rule" ++ show rule) (executeRule rulesMap str rule) in
---             case r of
---                 Nothing -> trace "baise ouais" Nothing 
---                 Just rest -> case restRules of
---                     [] -> Just rest
---                     _ -> trace ("Continuing with " ++ show restRules) executeRules rulesMap rest [restRules]
+executeSeqRules :: Map.Map Int [[Op]] -> [Op] -> [Char] -> [Maybe [Char]]
+executeSeqRules rulesMap [rule] s = executeRule rulesMap rule s
+executeSeqRules rulesMap (rule:restRule) s =
+    let 
+        results = executeRule rulesMap rule s
+        filteredResults = catMaybes results  
+    in
+        concat $ map (executeSeqRules rulesMap restRule) filteredResults 
 
 checkAgainstRule rulesMap s = 
-    let result = executeRules rulesMap s (fromJust $ Map.lookup 0 rulesMap) [] in
-        result 
+    let result = executeOrRules rulesMap (fromJust $ Map.lookup 0 rulesMap) [s] in
+        (Just "" `elem` result) 
 
 count x = length . filter (==x)
 
@@ -84,16 +60,11 @@ rulesSecondPart = Map.fromList [
 
 main :: IO ()
 main = do
-    -- file <- readFile "data/test19.txt"
-    -- putStrLn "Day 19"
-    -- putStrLn "Part 1"
-    -- let (rules, strings) = parseInput file
-    -- print $ count True $ map (checkAgainstRule rules) strings
-    putStrLn "Part 2"
-    file <- readFile "data/test19-set.txt"
+    file <- readFile "data/test19.txt"
+    putStrLn "Day 19"
+    putStrLn "Part 1"
     let (rules, strings) = parseInput file
+    print $ count True $ map (checkAgainstRule rules) strings
+    putStrLn "Part 2"
     let newRules = Map.union rulesSecondPart rules
-    -- print $ sortBy (\x y -> compare (fst x) (fst y)) $ Map.assocs newRules 
-    print $ map (checkAgainstRule newRules) strings
-    -- print $ checkAgainstRule newRules "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"
-    --print $ map (\s -> (checkAgainstRule newRules s, s)) strings 
+    print $ count True $ map (checkAgainstRule newRules) strings
