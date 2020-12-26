0bf0bc1ff1684e626c03cc0d09041af870981cfb
Jean Guegant <jean.guegant@king.com>
2020-12-26 19:57:35 +0100

    Started to build puzzle.

--- app/Day20/Main.hs
+++ app/Day20/Main.hs
@@ -8,13 +8,13 @@ import Data.Maybe
 import Data.List (transpose, groupBy)
 import qualified Data.Map as Map
 
-data BorderName = UpBorder | RightBorder | BottomBorder | LeftBorder deriving (Show)
+data BorderName = UpBorder | RightBorder | BottomBorder | LeftBorder deriving (Show, Eq)
 
 allBorderNames = [UpBorder, RightBorder, BottomBorder, LeftBorder]
 
 data PieceBorders = PieceBorders Word16 Word16 Word16 Word16 deriving (Show)
 
-data Piece = Piece PieceBorders [String] deriving (Show)
+data Piece = Piece { pieceId :: Int, borders :: PieceBorders, shape :: [String] } deriving (Show)
 
 sideToNumber :: [Char] -> Word16
 sideToNumber = foldl (\acc x -> acc * 2 + (if x == '#' then 1 else 0)) 0 
@@ -63,7 +63,7 @@ parsePiece line =
         bottom = sideToNumber $ last shape
         left = sideToNumber $ map head shape
     in
-        (pieceNumber, Piece (PieceBorders up right bottom left) (map (init . tail) $ init $ tail shape))
+        (pieceNumber, Piece pieceNumber (PieceBorders up right bottom left) (map (init . tail) $ init $ tail shape))
 
 parsePuzzle :: String -> [(Int, Piece)]
 parsePuzzle = map parsePiece . splitOn [""] . lines 
@@ -80,34 +80,77 @@ matchInAllDirections piece1 piece2 =
     let matches = map (\f -> let rotatedPiece2 = rotateBorders f piece2 in (match piece1 rotatedPiece2, f)) allPossibleRotationFunc in
         filter (\(m, _) -> not $ null m) matches
 
-matchPieceWithOthers :: Map.Map Int Piece -> (Int, Piece) -> [(Int, [([BorderName], RotateFuncs)])]
-matchPieceWithOthers pieces (pieceId, Piece borders _) = 
-    [(pieceId2, matchInAllDirections borders borders2) | (pieceId2, Piece borders2 _) <- Map.assocs pieces, pieceId /= pieceId2]
+matchPieceWithOthers :: Map.Map Int Piece -> Piece -> [(Int, [([BorderName], RotateFuncs)])]
+matchPieceWithOthers pieces (Piece pieceId borders _) = 
+    let
+        matches = [(pieceId2, matchInAllDirections borders borders2) | Piece pieceId2 borders2 _ <- Map.elems pieces, pieceId /= pieceId2]
+    in
+        filter (not . null . snd) matches
 
-matchAllPieces :: Eq a => Map.Map Int Piece -> [(a, [(a, [([BorderName], PieceBorders -> PieceBorders)])])]
+matchAllPieces :: Map.Map Int Piece -> [(Int, [(Int, [([BorderName], RotateFuncs)])])]
 matchAllPieces pieces = 
     let 
-        allMatches = [matchPieceWithOthers pieces piece | piece <- Map.assocs pieces]
-        nonEmptyMatches = filter (\(_, ( _, matches)) -> not $ null matches) allMatches
-        groupedMatches = groupBy (\x y -> fst x == fst y) nonEmptyMatches
+        allMatches = [(pieceId piece, matchPieceWithOthers pieces piece) | piece <- Map.elems pieces]
     in
-        map (\(s:xs) -> (fst s, map snd (s:xs))) groupedMatches
+        allMatches
 
-constructFirstRow puzzle upperLeftCorner =
-    upperLeftCorner
+findPieceMatching :: Map.Map Int Piece -> Piece -> BorderName -> [(Int, [RotateFuncs])]
+findPieceMatching pieces piece border = 
+    let
+        matchingPieces = matchPieceWithOthers pieces piece 
+        matchingBorders = map (\(id, matches) -> (id, filter (\(borders, _) -> border `elem` borders) matches)) matchingPieces
+        filtered = filter (not . null . snd) matchingBorders
+    in 
+       map (\(id, borders) -> (id, map snd borders)) filtered 
+
+constructFirstRow :: Map.Map Int Piece -> [Piece] -> ([Piece], Map.Map Int Piece)
+constructFirstRow pieces row =
+    let
+        rightMostPiece = last row 
+        matchingPieces = findPieceMatching pieces rightMostPiece RightBorder
+    in
+        case matchingPieces of
+            [] -> (row, pieces) 
+            [(pieceId, [rotateFuncs])] ->
+                let
+                    Just (Piece id borders shape) = Map.lookup pieceId pieces
+                    newPiece = Piece id (rotateBorders rotateFuncs borders) (rotateContent rotateFuncs shape)
+                in
+                constructFirstRow (Map.delete id pieces) (row ++ [newPiece])
+
+constructRowsBellow :: Map.Map Int Piece -> [[Piece]] -> [[Piece]]
+constructRowsBellow pieces rows 
+    | Map.null pieces = rows
+    | otherwise =
+        let
+            lastRow = last rows
+            newRow = map (\piece -> findPieceMatching pieces piece) lastRow
+        in
+            newRow
+
+
+drawPuzzle :: [[Piece]] -> [String]
+drawPuzzle = concatMap (map concat . transpose . map shape)
+
+printPuzzle :: [[Piece]] -> IO ()
+printPuzzle =
+    mapM_ (mapM_ (print . shape)) 
 
 main :: IO ()
 main = do
-    file <- readFile "data/test20.txt"
+    file <- readFile "data/test20-set2.txt"
     putStrLn "Day 20"
     putStrLn "Part 1"
-    let puzzle = Map.fromList $ parsePuzzle file
-    print $ puzzle 
-    let allMatches = matchAllPieces puzzle 
+    let pieces = Map.fromList $ parsePuzzle file
+    print pieces 
+    let allMatches = matchAllPieces pieces 
+    print allMatches
     let corners = filter ((==2) . length . snd) allMatches
     print $ product $ map fst corners 
     putStrLn "Part 2"
     let upperLeftBorderMatch = [RightBorder, BottomBorder]
-    let Just upperLeftCorner = find (\(id, [(_, [([s1], _)]), (_, [([s2], _)])]) -> s1 `elem` upperLeftBorderMatch && s2 `elem` upperLeftBorderMatch) corners 
+    let Just upperLeftCornerId = find (\(_, [(_, [([s1], _)]), (_, [([s2], _)])]) -> s1 `elem` upperLeftBorderMatch && s2 `elem` upperLeftBorderMatch) corners 
+    let Just upperLeftCorner = Map.lookup (fst upperLeftCornerId) pieces
     print upperLeftCorner
-    -- print $ constructFirstRow allMatchesList upperLeftCorner 
+    let (firstRow, remainingPieces) = constructFirstRow pieces [upperLeftCorner] 
+    print $ drawPuzzle [firstRow]
