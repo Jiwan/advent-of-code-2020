c94a8d7ae85592db2bfafdf1867a9a7f985a7c70
Jean Guegant <jean.guegant@king.com>
2020-12-28 18:22:06 +0100

    Finished day 22.

--- app/Day22/Main.hs
+++ app/Day22/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.List.Split (splitOn)
+import qualified Data.Set as Set
 
 tuplify [x,y] = (x,y)
 
@@ -14,6 +15,23 @@ play (s1:xs1, s2:xs2)
     | s1 > s2 = play (xs1 ++ [s1, s2], xs2)
     | s1 < s2 = play (xs1, xs2 ++ [s2, s1])
 
+playRecursive :: ([Int], [Int]) -> Set.Set (Int, Int) -> ([Int], [Int])
+playRecursive ([], r) s = ([], r)
+playRecursive (r, []) s = (r, [])
+playRecursive (s1:xs1, s2:xs2) roundSet 
+    | hasAlreadyBeenPlayed = ([0], [])
+    | s1 <= (length xs1) && s2 <= (length xs2) =
+        case playRecursive (take s1 xs1, take s2 xs2) Set.empty of
+            (_, []) -> playRecursive (xs1 ++ [s1, s2], xs2) newRoundSet
+            ([], _) -> playRecursive (xs1, xs2 ++ [s2, s1]) newRoundSet
+    | s1 > s2 = playRecursive (xs1 ++ [s1, s2], xs2) newRoundSet
+    | s1 < s2 = playRecursive (xs1, xs2 ++ [s2, s1]) newRoundSet 
+    where
+        hash l = sum $ zipWith (*)  [1..] l
+        hashOfRound = (hash (s1:xs1), hash (s2:xs2))
+        hasAlreadyBeenPlayed = Set.member hashOfRound roundSet
+        newRoundSet = Set.insert hashOfRound roundSet
+
 main :: IO ()
 main = do
     file <- readFile "data/test22.txt"
@@ -22,3 +40,6 @@ main = do
     let decks = parseDecks file
     let result = play decks
     print $ sum $ zipWith (*) (reverse result) [1..] 
+    putStrLn "Part 2"
+    let result2 = playRecursive decks Set.empty
+    print $ sum $ zipWith (*) (reverse $ fst result2) [1..]
