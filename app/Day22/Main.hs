8e6eda19aae3e190c7bf8425ed810354f01e61a7
Jean Guegant <jean.guegant@king.com>
2020-12-28 17:40:30 +0100

    Started day 22 part 1.

--- app/Day22/Main.hs
+++ app/Day22/Main.hs
@@ -1,46 +1,24 @@
 module Main where
 
-import Data.List.Split (splitOneOf, wordsBy, splitOn)
-import Data.Char (isSpace)
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import Data.List (intersperse, sortBy, find)
-import Data.Function (on)
+import Data.List.Split (splitOn)
 
-parseFood :: String -> ([String], [String])
-parseFood line = 
-    let [ingredients, allergens] = splitOn "(contains " line in
-        (wordsBy isSpace ingredients, filter (not . null) $ splitOneOf ", )" allergens)
+tuplify [x,y] = (x,y)
 
-parseFoods :: String -> [([String], [String])]
-parseFoods = map parseFood . lines 
+parseDecks :: String -> ([Int], [Int])
+parseDecks = tuplify . map (map read . tail) . splitOn [""] . lines
 
-createAllergenMap :: [([String], [String])] -> Map.Map String (Set.Set String)
-createAllergenMap = foldr addIngredientsForAllergens Map.empty
-    where
-        addIngredientsForAllergens (ingredients, allergens) map = foldr (addIngredientForAllergen ingredients) map allergens
-        addIngredientForAllergen ingredients allergen map = Map.insert allergen (Set.intersection (Set.fromList ingredients) (Map.findWithDefault (Set.fromList ingredients) allergen map)) map
-
-figureOutAllergens :: Map.Map String (Set.Set String) -> [(String, String)]
-figureOutAllergens m 
-    | Map.null m = []
-    | otherwise = 
-        (allergen, ingredient):(figureOutAllergens restOfMap)
-        where
-            Just (allergen, set) = find ((==1) . length . snd) (Map.assocs m)
-            ingredient = Set.elemAt 0 set
-            removeAllergenMap = Map.delete allergen m
-            restOfMap = Map.map (Set.delete ingredient) removeAllergenMap
+play :: ([Int], [Int]) -> [Int]
+play ([], r) = r
+play (r, []) = r
+play (s1:xs1, s2:xs2)
+    | s1 > s2 = play (xs1 ++ [s1, s2], xs2)
+    | s1 < s2 = play (xs1, xs2 ++ [s2, s1])
 
 main :: IO ()
 main = do
-    file <- readFile "data/test21.txt"
-    putStrLn "Day 21"
+    file <- readFile "data/test22.txt"
+    putStrLn "Day 22"
     putStrLn "Part 1"
-    let foods = parseFoods file
-    let allergenMap = createAllergenMap foods
-    let safeIngredients = Set.difference (Set.fromList $ concat $ map fst foods) (foldl Set.union Set.empty (Map.elems allergenMap))
-    let dangerousIngredients = length . filter (flip Set.member safeIngredients) $ (concat $ map fst foods)
-    print dangerousIngredients
-    putStrLn "Part 2"
-    mapM_ putStr $ intersperse "," $ map snd $ sortBy (compare `on` fst) $ figureOutAllergens allergenMap
+    let decks = parseDecks file
+    let result = play decks
+    print $ sum $ zipWith (*) (reverse result) [1..] 
