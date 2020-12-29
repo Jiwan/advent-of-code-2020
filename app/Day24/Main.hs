da4a920edc4781d7f90bbb52f44358325ee7a785
Jean Guegant <jean.guegant@king.com>
2020-12-29 20:29:09 +0100

    Finished day 24.

--- app/Day24/Main.hs
+++ app/Day24/Main.hs
@@ -21,13 +21,40 @@ move (x, y) E = (x + 2, y)
 move (x, y) SE = (x + 1, y - 1)
 move (x, y) NE = (x + 1, y + 1)
 
-prepareTiles :: [[Direction]] -> Set.Set (Int, Int)
-prepareTiles =
+computeBlackTiles :: [[Direction]] -> Set.Set (Int, Int)
+computeBlackTiles =
     foldl updateBlackTiles Set.empty
     where
         updateBlackTiles set tilePath =  let reachedTile = foldl move (0, 0) tilePath in 
             if Set.member reachedTile set then Set.delete reachedTile set else Set.insert reachedTile set
 
+getNeighboors coords = map (move coords) [W, SW, NW, E, SE, NE] 
+
+count x = length . filter (==x)
+
+spawnToLife world newWorld coord =
+    let
+        neighboors = getNeighboors coord 
+        aliveNeighboor = count True $ map (`Set.member` world) neighboors
+        spawn = aliveNeighboor == 2
+    in
+        if spawn then Set.insert coord newWorld else newWorld
+
+keepAliveRule world newWorld coord = 
+    let
+        neighboors = getNeighboors coord 
+        aliveNeighboor = count True $ map (`Set.member` world) neighboors
+        stayAlive = aliveNeighboor >= 1 && aliveNeighboor <= 2
+    in
+        if stayAlive then Set.insert coord newWorld else newWorld
+
+playWorld world 0 = world 
+playWorld world step = 
+    let 
+        keptAlive = foldl (keepAliveRule world) Set.empty world
+        newOnes = foldl (\newWorld cell -> foldl (spawnToLife world) newWorld (getNeighboors cell)) Set.empty world
+    in 
+        playWorld (Set.union keptAlive newOnes) (step - 1)
 
 main :: IO ()
 main = do
@@ -35,4 +62,6 @@ main = do
     putStrLn "Day 24"
     putStrLn "Part 1"
     let tiles = parseTiles file
-    print $ length $ prepareTiles tiles
+    let blackTiles = computeBlackTiles tiles
+    print $ length $ blackTiles 
+    print $ length $ playWorld blackTiles 100 
